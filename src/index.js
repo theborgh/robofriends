0448b0b41377aa2efa12957caed8025806bc3dbe
theborgh <borghiborghi@gmail.com>
2019-03-02 00:15:05 +0100

    Add hello component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting = {'Salutations' + ' you dumbass'}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
