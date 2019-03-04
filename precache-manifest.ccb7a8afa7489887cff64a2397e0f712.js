ea72f74d600deb354d8889a62bea84b77b263b34
theborgh <borghiborghi@gmail.com>
2019-03-04 13:15:03 +0100

    Updates

---
+++ precache-manifest.ccb7a8afa7489887cff64a2397e0f712.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robobook/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a2dfd5af0daf3a0581cc",
+    "url": "/robobook/static/js/runtime~main.a2dfd5af.js"
+  },
+  {
+    "revision": "f6490e619f33127e0eeb",
+    "url": "/robobook/static/js/main.f6490e61.chunk.js"
+  },
+  {
+    "revision": "5785b0cf4573198deb21",
+    "url": "/robobook/static/js/2.5785b0cf.chunk.js"
+  },
+  {
+    "revision": "f6490e619f33127e0eeb",
+    "url": "/robobook/static/css/main.da4b71cd.chunk.css"
+  },
+  {
+    "revision": "5785b0cf4573198deb21",
+    "url": "/robobook/static/css/2.6bd0b5a3.chunk.css"
+  },
+  {
+    "revision": "c69aa9c75434c7988cb8c1a3da700351",
+    "url": "/robobook/index.html"
+  }
+];
